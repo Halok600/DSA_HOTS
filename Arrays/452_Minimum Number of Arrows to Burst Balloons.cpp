e956db9a7989da2ac8c828d49dfb8390275849e9
halok600 <143282195+Halok600@users.noreply.github.com>
2026-02-28 02:06:46 +0530

    || Solved problem 452 ||

--- Arrays/452_Minimum Number of Arrows to Burst Balloons.cpp
+++ Arrays/452_Minimum Number of Arrows to Burst Balloons.cpp
@@ -1,2 +1,24 @@
-Halo is best
-bye bye
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(),points.end());
+        int n = points.size();
+        int prevSP = points[0][0];
+        int prevEP = points[0][1];
+        int arrows = 1;
+        for(int i = 1;i<n;i++){
+            int currSP = points[i][0];
+            int currEP = points[i][1];
+            if(currSP>prevEP){
+                prevSP = currSP;
+                prevEP = currEP;
+                arrows++;
+            }
+            else{
+                prevSP = max(currSP,prevSP);
+                prevEP = min(currEP,prevEP);
+            }
+        }
+        return arrows;
+    }
+};
