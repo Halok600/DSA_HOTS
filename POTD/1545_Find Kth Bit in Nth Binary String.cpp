96a0c1bfcb551b8eb0b8922d739972677712c662
halok600 <143282195+Halok600@users.noreply.github.com>
2026-03-03 21:55:21 +0530

    || SOLVED with Optimized Approach ||

--- POTD/1545_Find Kth Bit in Nth Binary String.cpp
+++ POTD/1545_Find Kth Bit in Nth Binary String.cpp
@@ -24,3 +24,24 @@ public:
         return x[n-1][k-1];
     }
 };
+
+// Optimized Recursive Approach
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        int len = pow(2,n)-1;
+        if(n==1)
+            return '0';
+        if(k<ceil(len/2.0))
+            return findKthBit(n-1,k);
+        else if(k==ceil(len/2.0))
+            return '1';
+        else{
+            char ch = findKthBit(n-1,len-(k-1));
+            if(ch=='0')
+                return '1';
+            else
+                return '0';
+        }
+    }
+};
