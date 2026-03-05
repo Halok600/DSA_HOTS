787dd4be26974174fb1e1930f89019b1af7937c4
halok600 <143282195+Halok600@users.noreply.github.com>
2026-03-05 22:27:43 +0530

    SOlved the problem with a better approach

--- POTD/1758_Minimum Changes To Make Alternating Binary String.cpp
+++ POTD/1758_Minimum Changes To Make Alternating Binary String.cpp
@@ -28,3 +28,47 @@ public:
         return min(ops1,ops2);
     }
 };
+
+//BETTER Approach saves Space
+class Solution {
+public:
+    int minOperations(string s) {
+        int op1 = 0;
+        int op2 = 0;
+        for(int i = 0;i<s.length();i++){
+            if(i%2==0){
+                if(s[i]=='1')
+                    op2++;
+                else
+                    op1++;
+            }
+            else{
+                if(s[i]=='0')
+                    op2++;
+                else
+                    op1++;
+            }
+        }
+        return min(op1,op2);
+    }
+};
+
+//BEST Approach Smallest code
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        int res=  0;
+        for(int i = 0;i<n;i++){
+            if(i%2==0){
+                if(s[i]=='0')
+                    res++;
+            }
+            else{
+                if(s[i]=='1')
+                    res++;
+            }
+        }
+        return min(res,n-res);
+    }
+};
