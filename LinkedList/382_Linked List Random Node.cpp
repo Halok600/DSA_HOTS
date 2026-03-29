dc0c6bcd01357ed2cf32ef8917c805859bf85fa3
Priyanshu Kumar Tiwari <143282195+Halok600@users.noreply.github.com>
2026-03-29 15:11:28 +0530

    Add Reservoir Sampling method to get random node
    Implemented Reservoir Sampling to randomly select a node value from the linked list.

--- LinkedList/382_Linked List Random Node.cpp
+++ LinkedList/382_Linked List Random Node.cpp
@@ -31,3 +31,41 @@ public:
  * Solution* obj = new Solution(head);
  * int param_1 = obj->getRandom();
  */
+// Second Appraoch Reservoir Sampling
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* Head;
+    Solution(ListNode* head) {
+        Head = head;
+    }
+    
+    int getRandom() {
+        ListNode* temp = Head;
+        int count = 1;
+        int res = 0;
+        while(temp!=NULL){
+            if((rand()%count)<(1.0/(count)))
+                res = temp->val;
+            temp = temp->next;
+            count++;
+
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
