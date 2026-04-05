1c4a05f0305a1831db8ca2ce1b63a5248c3bf68a
Priyanshu Kumar Tiwari <143282195+Halok600@users.noreply.github.com>
2026-04-05 09:09:59 +0530

    || Solved Problem 111 ||

---
+++ Binary Trees/111_Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int res = 1;
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0;i<n;i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->right == NULL && temp->left==NULL)
+                    return res;
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+            }
+            res++;
+        }
+        return res;
+    }
+};
