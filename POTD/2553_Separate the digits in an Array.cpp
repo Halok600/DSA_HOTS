2218c07f02c017861c72d6d62ba89acada5ead6d
Priyanshu Kumar Tiwari <143282195+Halok600@users.noreply.github.com>
2026-05-11 21:07:07 +0530

    Implement Python solution for digit separation
    Added a Python approach for separating digits from an array.

--- POTD/2553_Separate the digits in an Array.cpp
+++ POTD/2553_Separate the digits in an Array.cpp
@@ -12,3 +12,12 @@ public:
         return res;
     }
 };
+//PYTHON APPROACH
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        res = []
+        for val in nums:
+            s = str(val)
+            for char in s:
+                res.append(int(char))
+        return res
