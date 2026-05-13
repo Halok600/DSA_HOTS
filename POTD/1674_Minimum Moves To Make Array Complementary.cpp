e4a31f7f7c2dd4122f0fc5f991de629aa728dbf2
Priyanshu Kumar Tiwari <143282195+Halok600@users.noreply.github.com>
2026-05-13 23:25:23 +0530

    Add brute force solution for minimum moves problem
    Implemented a brute force approach to calculate minimum moves needed to make the array complementary based on given limits.

--- POTD/1674_Minimum Moves To Make Array Complementary.cpp
+++ POTD/1674_Minimum Moves To Make Array Complementary.cpp
@@ -1,1 +1,28 @@
 //C++  SOLUTION COMING SOON
+//BRUTE FORCE APPROACH
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n = nums.size();
+        int res = INT_MAX;
+        for(int sum = 2;sum<=2*limit;sum++){
+            int moves = 0;
+            for(int i = 0;i<n/2;i++){
+                int a = nums[i];
+                int b = nums[n-i-1];
+                if(sum==a+b)
+                    continue;
+                int minVal = min(a,b)+1;
+                int maxVal = max(a,b) + limit;
+                if(sum<=maxVal && sum>=minVal){
+                    moves+=1;
+                }
+                else
+                    moves+=2;
+            }
+            res = min(res,moves);
+        }
+        return res;
+        
+    }
+};
